b084c93b336abdab1a8d4693792a6f03a0ce2a2c
Huzaifa Azhar <87518251+BloggingKIng@users.noreply.github.com>
2022-08-11 17:00:57 +0500

    SECRET KEY
    removed secret key so someone using this project will may not get into trouble

--- CovidClassifier/settings.py
+++ CovidClassifier/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q8g9jgrlmzq-r3#)=+py+r(087zdcbit8w=7n8i#^g+s6ti^dk'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
